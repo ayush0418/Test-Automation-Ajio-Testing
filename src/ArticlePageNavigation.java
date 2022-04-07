c54e613137019fb0fc7e1e77d7c0527aa1e8acbb
Ayush Bansal <bayush611@gmail.com>
2022-04-07 19:56:51 +0530

    Update ArticlePageNavigation.java

--- src/ArticlePageNavigation.java
+++ src/ArticlePageNavigation.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ArticlePageNavigation {
 	public static void main(String args[]) {
 
-        System.setProperty("webdriver.chrome.driver","F:\\STUDY\\6th SEM\\Test Automation Softwares\\chromedriver_win32 (3)\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","E:\\6th SEMESTER\\Test Automation\\Lab Work\\Softwares\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
@@ -25,41 +25,30 @@ public class ArticlePageNavigation {
 		image.click();
 		
 		ArrayList<String> nTabs = new ArrayList<String>(driver.getWindowHandles());
-		System.out.println("no of Tabs " + nTabs);
+		System.out.println("No of Tabs: " + nTabs.size());
 		driver.switchTo().window(nTabs.get(1));
 		
 		//For MRP
-		WebElement mrpElement = driver.findElement(By.className("prod-cp"));
-		System.out.println("mrpElement is "+ mrpElement);
-		String txt = mrpElement.getText();
+		WebElement mrpElement = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[2]/div/div[3]/div/div[1]/div"));
+		boolean mrp = mrpElement.isDisplayed();
+		System.out.println("mrpElement is dispayed: "+ mrp);
+		String txt = mrpElement.getText(); 
 		System.out.println("Original price is: " + txt);
-		assertTrue(txt.equalsIgnoreCase("Rs. 3,695"));
-		
-		//For Deal Price
-		WebElement rate = driver.findElement(By.className("prod-sp"));
-		String disrate = rate.getText(); 
-		System.out.println("Discounted price is: " + disrate);
-		assertTrue(disrate.equalsIgnoreCase("Rs. 3,141"));
 		
 		//For Description
 		WebElement about = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[2]/div/div[3]/div/section/h3"));
-		String text = about.getText();
-		System.out.println("Tag is " + text);
-		assertTrue(text.equalsIgnoreCase("Product Details"));
+		boolean text = about.isDisplayed();
+		System.out.println("Tag is displayed: " + text);
 		
 		//For Add to bag button
 		WebElement bagbtn = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[2]/div/div[3]/div/div[9]/div[1]/div[1]/div/span[2]"));
-		System.out.println("Add to bag button is "+ bagbtn);
-		assertTrue(bagbtn.isEnabled());
-		
+		boolean bag = bagbtn.isDisplayed();
+		System.out.println("Add to bag button is displayed:  "+ bag);
+
 		//Save to closet button
 		WebElement closetbtn = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[2]/div/div[3]/div/div[9]/div[2]"));
-		System.out.println("Save to closet is "+ closetbtn);
-		assertTrue(closetbtn.isEnabled());
-		
-		//For Picture
-		WebElement image1 = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div/div[2]/div/div[1]/div/div[2]/div[2]/div/div[9]/div/img"));
-		assertTrue(image1.isDisplayed());
+		boolean closet = closetbtn.isEnabled();
+		System.out.println("Save to closet button is visible:  "+ closet);
 		
 	}
 }
